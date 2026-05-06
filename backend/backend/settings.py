21a89a9a2eb1059bf3f7d2b794a349ca9768e273
Tania Cruz <tania.crz.crz96@gmail.com>
2026-05-06 12:35:18 -0600

    Cambiar proveedor de correo a Brevo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,12 +125,12 @@ STORAGES = {
 
 # Configuración de correo
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465  
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True  
-EMAIL_TIMEOUT = 20
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 30
+EMAIL_HOST_USER = 'tania.crz.crz96@gmail.com'  
+EMAIL_HOST_PASSWORD = os.environ.get('BREVO_API_KEY')  
+DEFAULT_FROM_EMAIL = 'tania.crz.crz96@gmail.com'  
+SERVER_EMAIL = 'tania.crz.crz96@gmail.com'
