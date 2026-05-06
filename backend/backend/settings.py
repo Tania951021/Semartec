11c01ab6204479b3fe0bc46e515e33fc446750d7
Tania Cruz <tania.crz.crz96@gmail.com>
2026-05-06 10:53:28 -0600

    Corregir configuración de WhiteNoise

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,11 +112,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Esta es la carpeta "static" que ya tienes en tu proyecto
+    BASE_DIR / 'static',
 ]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
