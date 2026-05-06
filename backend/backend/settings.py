8a9087135edf9bf3b020e5e0f0ccf9b3551f4387
Tania Cruz <tania.crz.crz96@gmail.com>
2026-05-06 09:28:09 -0600

    Agregar whitenoise a dependencias

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,3 +124,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tania.crz.crz96@gmail.com'
 EMAIL_HOST_PASSWORD = 'szgycuupqhfzoqbw'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
